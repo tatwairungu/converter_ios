ebb6c69fa0881698bb3bdb24b7cd4e5d0accbeda
tatwairungu <tatwairungu@icloud.com>
2025-08-15 22:22:19 +0300

    Initial commit

---
+++ cursor_test/cursor_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  cursor_testApp.swift
+//  cursor_test
+//
+//  Created by Tatwa on 14/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct cursor_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ cursor_testTests/cursor_testTests.swift
@@ -0,0 +1,17 @@
+//
+//  cursor_testTests.swift
+//  cursor_testTests
+//
+//  Created by Tatwa on 14/08/2025.
+//
+
+import Testing
+@testable import cursor_test
+
+struct cursor_testTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
